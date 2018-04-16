a6ec75b5c17b462e21782128d58175413c626a8f
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-04-16 11:47:14 +0200

    test gitignore

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -9,6 +11,7 @@ public class App
     public static void main( String[] args )
     {
     	//int i = 1/0;
+    	System.out.println(FigletFont.convertOneLine("Hello World!"));
         System.out.println( "Hello World!" );
     }
 }
